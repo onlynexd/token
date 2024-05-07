57ff8f329ab2df505357e83ec81b5d74ee1036b3
nftgrinder3 <nftgrinder3@gmail.com>
2024-05-07 17:56:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
+const bot = new Eris(process.env.token2);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
