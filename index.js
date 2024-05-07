3a2a4cabfb3038b115407f6bfc59f0e7b7993a1f
nftgrinder3 <nftgrinder3@gmail.com>
2024-05-08 00:00:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const keep_alive = require('./keep_alive.js')
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 const bot1 = new Eris(process.env.token2);
+const bot2 = new Eris(process.env.token3);
+const bot3 = new Eris(process.env.token4);
+const bot4 = new Eris(process.env.token5);
+const bot5 = new Eris(process.env.token6);
+const bot6 = new Eris(process.env.token7);
+const bot7 = new Eris(process.env.token8);
+const bot8 = new Eris(process.env.token9);
+const bot9 = new Eris(process.env.token10);
+const bot10 = new Eris(process.env.token11);
+const bot11 = new Eris(process.env.token12);
+const bot12 = new Eris(process.env.token13);
+const bot13 = new Eris(process.env.token14);
+const bot14 = new Eris(process.env.token15);
+const bot15 = new Eris(process.env.token16);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
@@ -11,6 +25,62 @@ bot.on("error", (err) => {
 bot1.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
+bot2.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot3.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot4.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot5.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot6.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot7.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot8.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot9.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot10.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot11.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot12.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot13.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot14.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot15.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
 
 bot.connect(); // Get the bot to connect to Discord
 bot1.connect(); // Get the bot to connect to Discord
+bot2.connect(); // Get the bot to connect to Discord
+bot3.connect(); // Get the bot to connect to Discord
+bot4.connect(); // Get the bot to connect to Discord
+bot5.connect(); // Get the bot to connect to Discord
+bot6.connect(); // Get the bot to connect to Discord
+bot7.connect(); // Get the bot to connect to Discord
+bot8.connect(); // Get the bot to connect to Discord
+bot9.connect(); // Get the bot to connect to Discord
+bot10.connect(); // Get the bot to connect to Discord
+bot11.connect(); // Get the bot to connect to Discord
+bot12.connect(); // Get the bot to connect to Discord
+bot13.connect(); // Get the bot to connect to Discord
+bot14.connect(); // Get the bot to connect to Discord
+bot15.connect(); // Get the bot to connect to Discord
