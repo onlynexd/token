91b12f17511ca0d351684ff45dec3c2fcb1a1af0
onlynexd <106020262+onlynexd@users.noreply.github.com>
2024-05-09 15:55:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
+const bot = new Eris(process.env.token);
+const bot1 = new Eris(process.env.token1);
+const bot2 = new Eris(process.env.token2);
+const bot3 = new Eris(process.env.token3);
+const bot4 = new Eris(process.env.token4);
+const bot5 = new Eris(process.env.token5);
+const bot6 = new Eris(process.env.token6);
+const bot7 = new Eris(process.env.token7);
 const bot8 = new Eris(process.env.token8);
 const bot9 = new Eris(process.env.token9);
 const bot10 = new Eris(process.env.token10);
@@ -12,6 +20,30 @@ const bot14 = new Eris(process.env.token14);
 const bot15 = new Eris(process.env.token15);
 const bot16 = new Eris(process.env.token16);
 
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot1.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot2.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot3.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot4.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot5.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot6.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot7.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
 bot8.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
@@ -40,6 +72,14 @@ bot16.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
+bot.connect(); // Get the bot to connect to Discord
+bot1.connect(); // Get the bot to connect to Discord
+bot2.connect(); // Get the bot to connect to Discord
+bot3.connect(); // Get the bot to connect to Discord
+bot4.connect(); // Get the bot to connect to Discord
+bot5.connect(); // Get the bot to connect to Discord
+bot6.connect(); // Get the bot to connect to Discord
+bot7.connect(); // Get the bot to connect to Discord
 bot8.connect(); // Get the bot to connect to Discord
 bot9.connect(); // Get the bot to connect to Discord
 bot10.connect(); // Get the bot to connect to Discord
